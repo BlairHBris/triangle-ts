2479a89b1cec2e93f9b384df4fc618b48b9ef5fe
Blair Bristow <blairhbristow@gmail.com>
2022-05-10 15:10:24 -0400

    Post kyle session

--- triangle.ts
+++ triangle.ts
@@ -1,4 +1,65 @@
-import { truncate } from "fs"
+interface ITriange {
+    isEquilateral: boolean
+    isIsosceles: boolean
+    isScalene: boolean
+}
+
+export class Triangle {
+    side1: number
+    side2: number
+    side3: number
+
+    constructor(side1: number, side2: number, side3: number) {
+        this.side1 = side1
+        this.side2 = side2
+        this.side3 = side3
+    }
+
+    get sideRules() {
+        return (
+            (this.side1 + this.side2) > this.side3 && 
+            (this.side1 + this.side3) > this.side2 && 
+            (this.side3 + this.side2) > this.side1
+        )
+    }
+
+    get isTriangle() {
+        return( 
+            this.sideRules &&
+            this.side1 > 0 && 
+            this.side2 > 0 && 
+            this.side3 > 0
+        )
+    }
+
+    get isEquilateral() {
+        return(
+            this.isTriangle && 
+            this.side1 === this.side2 && 
+            this.side1 === this.side3
+        )
+    }
+
+    get isIsosceles() {
+        return(
+            this.isTriangle && 
+            (this.side1 === this.side2 || 
+            this.side1 === this.side3 || 
+            this.side2 === this.side3)
+        )
+    }
+
+    get isScalene() {
+        return(
+            this.isTriangle && 
+            !(this.side1 === this.side2 || 
+            this.side1 === this.side3 || 
+            this.side2 === this.side3)
+        )
+    }
+}
+
+/*import { truncate } from "fs"
 
 export class Triangle {
     sideOne: number
@@ -31,4 +92,5 @@ export class Triangle {
             this.isScalene = false
         }
     }
-}
+} 
+*/
